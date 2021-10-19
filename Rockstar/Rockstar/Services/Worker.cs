93e734b63c3f23e8a231928a39550976925e1b72
Yonatan Cohen <yec1997@gmail.com>
2021-10-19 19:25:45 +0300

    added functionality

--- Rockstar/Rockstar/Services/Worker.cs
+++ Rockstar/Rockstar/Services/Worker.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NewsAPI;
+using NewsAPI.Constants;
 using NewsAPI.Models;
 using Rockstar.Models;
 using System;
@@ -17,12 +18,13 @@ namespace Rockstar.Services
         private IConfiguration _configuration;
         private readonly IServiceProvider _provider;
         private RockStarDbContext _rockStarDbContext;
-        private DateTime _oneWeekAgo = DateTime.Now.AddDays(-(int) DateTime.Now.DayOfWeek - 7);
+        private DateTime _traceBackDateForArticels;
 
         public Worker(IServiceProvider serviceProvider, IConfiguration configuration)
         {
             _provider = serviceProvider;
             _configuration = configuration;
+            _traceBackDateForArticels = DateTime.Now.AddDays(-(int)DateTime.Now.DayOfWeek - int.Parse(_configuration.GetSection("TraceBackArticleUpdateInDays").Value));
             _newsApiClient = new NewsApiClient(_configuration.GetSection("NewsApiKey").Value);
             
             Initialize();
@@ -32,19 +34,38 @@ namespace Rockstar.Services
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                //Models.Source c = new Models.Source(){
-                //    Id = "test",
-                //    Name = "test author",
-                //    Category = "test title",
-                //    Description = "test desc",
-                //    Url = "test url",
-                //    Country = "test img",
-                //    Language = "test content"
-                //};
-                //_rockStarDbContext.Source.Add(c);
-                //_rockStarDbContext.SaveChanges();
-                string a = _configuration.GetSection("NewsApiKey").Value;
-                await Task.Delay(5000);
+                ArticlesResult articlesResponse = _newsApiClient.GetEverything(new EverythingRequest
+                {
+                    Q = _configuration.GetSection("KeyWordFilter").Value,
+                    SortBy = SortBys.Popularity,
+                    Language = Languages.EN,
+                    From = _traceBackDateForArticels
+                });
+                if (articlesResponse.Status == Statuses.Ok)
+                {
+                    foreach(NewsAPI.Models.Article article in articlesResponse.Articles)
+                    {
+                        if (_rockStarDbContext.Article.Where(dbArticle => dbArticle.Url == article.Url).Count() == 0)
+                        {
+                            _rockStarDbContext.Add(new Models.Article()
+                            {
+                                Source = article.Source.Id,
+                                Author = article.Author,
+                                Title = article.Title,
+                                Description = article.Description,
+                                Url = article.Url,
+                                UrlToImage = article.UrlToImage,
+                                PublishedAt = article.PublishedAt,
+                                Content = article.Content
+                            });
+                        }
+                    }
+
+                _rockStarDbContext.SaveChanges();
+
+                }
+
+                await Task.Delay(int.Parse(_configuration.GetSection("NewsRefreshInterval").Value));
             }
         }
 
@@ -53,75 +74,52 @@ namespace Rockstar.Services
             using (IServiceScope scope = _provider.CreateScope())
             {
                 _rockStarDbContext = scope.ServiceProvider.GetService<RockStarDbContext>();
-                //Models.Source c = new Models.Source()
-                //{
-                //    Id = "test",
-                //    Name = "test author",
-                //    Category = "test title",
-                //    Description = "test desc",
-                //    Url = "test url",
-                //    Country = "test img",
-                //    Language = "test content"
-                //};
-                //_rockStarDbContext.Source.Add(c);
 
-                //_rockStarDbContext.SaveChanges();
-                try
+                if (_rockStarDbContext.Source.FirstOrDefault() == null)
                 {
-                    Models.Source c = _rockStarDbContext.Source.FirstOrDefault();
-                }
-                catch (Exception e)
-                {
+                    foreach (NewsAPI.Models.Source source in _newsApiClient.GetAllSources().Result.Sources)
+                    {
+                        _rockStarDbContext.Add(new Models.Source()
+                        {
+                            Id = source.Id,
+                            Name = source.Name,
+                            Description = source.Description,
+                            Url = source.Url,
+                            Category = source.Category,
+                            Language = source.Language,
+                            Country = source.Country
+                        });
+                    }
 
-                    throw;
                 }
-                
+
+                if (_rockStarDbContext.Article.FirstOrDefault() == null)
+                {
+                    EverythingRequest everythingRequest = new EverythingRequest()
+                    {
+                        Q = _configuration.GetSection("KeyWordFilter").Value,
+                        SortBy = NewsAPI.Constants.SortBys.PublishedAt,
+                        Language = NewsAPI.Constants.Languages.EN,
+                        From = _traceBackDateForArticels
+                    };
+                    foreach (NewsAPI.Models.Article article in _newsApiClient.GetEverything(everythingRequest).Articles)
+                    {
+                        _rockStarDbContext.Add(new Models.Article()
+                        {
+                            Source = article.Source.Id,
+                            Author = article.Author,
+                            Title = article.Title,
+                            Description = article.Description,
+                            Url = article.Url,
+                            UrlToImage = article.UrlToImage,
+                            PublishedAt = article.PublishedAt,
+                            Content = article.Content
+                        });
+                    }
+                }
+
+                _rockStarDbContext.SaveChanges();
             }
-            
-            //if (_rockStarDbContext.Source.FirstOrDefault() == null)
-            //{
-            //    foreach (NewsAPI.Models.Source source in _newsApiClient.GetAllSources().Result.Sources)
-            //    {
-            //        _rockStarDbContext.Add(new Models.Source()
-            //        {
-            //            Id = source.Id,
-            //            Name = source.Name,
-            //            Description = source.Description,
-            //            Url = source.Url,
-            //            Category = source.Category,
-            //            Language = source.Language,
-            //            Country = source.Country
-            //        });
-            //    }
-
-            //}
-
-            //if (_rockStarDbContext.Article.FirstOrDefault() == null)
-            //{
-            //    EverythingRequest everythingRequest = new EverythingRequest()
-            //    {
-            //        Q = _configuration.GetSection("KeyWordFilter").Value,
-            //        SortBy = NewsAPI.Constants.SortBys.PublishedAt,
-            //        Language = NewsAPI.Constants.Languages.EN,
-            //        From = _oneWeekAgo
-            //    };
-            //    foreach (NewsAPI.Models.Article article in _newsApiClient.GetEverything(everythingRequest).Articles)
-            //    {
-            //        _rockStarDbContext.Add(new Models.Article()
-            //        {
-            //            Source = article.Source.Id,
-            //            Author = article.Author,
-            //            Title = article.Title,
-            //            Description = article.Description,
-            //            Url = article.Url,
-            //            UrlToImage = article.UrlToImage,
-            //            PublishedAt = article.PublishedAt,
-            //            Content = article.Content
-            //        });
-            //    }
-            //}
-
-            
         }
     }
 }
